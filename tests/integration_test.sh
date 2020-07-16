90ae2d29552af806882706f697ab40f7cd7a910c
John R <jfr10@le.ac.uk>
2020-07-16 13:00:43 +0100

    Fixing typo in integration test

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -9,12 +9,18 @@ echo Testing tropomi_ut_no2...
 python ../uptrop/tropomi_ut_no2.py --trop_dir $TROPOMI_DIR --out_dir $OUT_DIR \
 --start_date $START_DATE --end_date $END_DATE --grid_res 4x5 --cloud_product fresco --cloud_threshold 07
 
+echo --------------------------------------
+
 echo Testing ut_no2_gc_test.py
 python ../uptrop/ut_no2_gc_test.py --gc_dir $GC_DIR --out_dir $OUT_DIR --start_date $START_DATE --end_date $END_DATE
+
+echo --------------------------------------
 
 echo Testing compare_tropomi_pandora.py
 python ../uptrop/compare_tropomi_pandora.py --trop_dir $TROPOMI_DIR --pan_dir $PANDORA_DIR --out_dir $OUT_DIR \
 --start_date $START_DATE --end_date $END_DATE--apply_bias_correction True
+
+echo --------------------------------------
 
 echo Testing fresco_cl_err.py
 python ../uptrop/fresco_cld_err.py --trop_dir $TROPOMI_DIR --out_dir $OUT_DIR \
